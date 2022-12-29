8b97d0a58b7ddcba76167820592d837f2ee1e8a8
Yurii Rybachok <drummer2009@ukr.net>
2022-12-29 12:43:03 +0200

    form for restaurant list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from "vue"
+
+const restaurantList = ref<string[]>([])
+const newRestaurantName = ref()
+
+function addRestaurant() {
+  restaurantList.value.push(newRestaurantName.value)
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <form @submit.prevent="addRestaurant">
+      <label for="restaurant-name">Restaurant Name</label>
+      <input id="restaurant-name" v-model="newRestaurantName" type="text" />
+      <button type="submit">Add Restaurant</button>
+    </form>
+    <ul>
+      <li 
+        v-for="restaurant in restaurantList" 
+        :key="restaurant">
+        {{ restaurant }}
+      </li>
+    </ul>
   </main>
 </template>
