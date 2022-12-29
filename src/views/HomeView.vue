5b14539aa817646ddf457afa4a2fff3180be47bf
Yurii Rybachok <drummer2009@ukr.net>
2022-12-29 17:10:05 +0200

    add basic restaurant form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,64 @@
 <script setup lang="ts">
 import { ref } from "vue"
 
-const restaurantList = ref<string[]>([])
-const newRestaurantName = ref()
+
+interface Restaurant {
+  name?: string
+  status?: RestaurantStatus
+  dishes?: Dish[]
+}
+
+type RestaurantStatus = 'Want to Try' | 'Recommended' | 'Do Not Recommended' | 'Must Try'
+
+const statusList = [
+  'Want to Try', 
+  'Recommended',
+  'Do Not Recommended',
+  'Must Try'
+]
+const restaurantList = ref<Restaurant[]>([])
+const newRestaurant = ref<Restaurant>({})
+
 
 function addRestaurant() {
-  restaurantList.value.push(newRestaurantName.value)
+  restaurantList.value.push({
+    name: newRestaurant.value.name,
+    status: 'Want to Try',
+    dishes: []
+  })
 }
 </script>
 
 <template>
   <main>
+    <pre>
+      {{ newRestaurant }}
+    </pre>
     <form @submit.prevent="addRestaurant">
-      <label for="restaurant-name">Restaurant Name</label>
-      <input id="restaurant-name" v-model="newRestaurantName" type="text" />
+      <div>
+        <label for="restaurant-name">Restaurant Name</label>
+        <input id="restaurant-name" v-model="newRestaurant.name" type="text" />
+      </div>
+      
+      <div>
+        <label for="restaurant-status">Restaurant Status</label>
+        <select 
+          name="restaurant-status" 
+          id="restaurant-status" 
+          v-model="newRestaurant.status"
+        >
+          <option 
+            v-for="status in statusList" 
+            :value="status"
+            :key="status"
+            >{{ status }}</option>
+        </select>
+      </div>
       <button type="submit">Add Restaurant</button>
     </form>
     <ul>
-      <li 
-        v-for="restaurant in restaurantList" 
-        :key="restaurant">
-        {{ restaurant }}
+      <li v-for="restaurant in restaurantList" :key="restaurant.name">
+        {{ restaurant.name }}
       </li>
     </ul>
   </main>
